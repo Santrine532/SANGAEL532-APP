83646d1e67e10c9b13730c3e2f59847c186eaa82
Santrine <santinemshila532@gmail.com>
2023-06-21 17:08:24 +0300

    created login signup home files

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import "./App.css";
+import Login from "./Components/Login";
+import Signup from "./Components/Signup";
+function App() {
+  return (
+    <>
+    <Signup/>
+      <Login/>
+      
+    </>
+  );
+}
+
+export default App;
